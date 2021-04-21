3059f2a0fee16c5acbcd7d285423b9f7618a9fca
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2021-04-21 15:14:12 +0300

    fix brew args

--- configure.sh
+++ configure.sh
@@ -26,7 +26,8 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
 #install ngrok
-brew cask install ngrok
+brew install --cask ngrok
+
 
 #configure ngrok and start it
 ngrok authtoken $3
